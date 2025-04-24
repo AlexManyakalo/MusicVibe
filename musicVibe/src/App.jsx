b33a5fd897d716918a5ed7c9404b4a9a6572a1bc
Alex06052003 <amm-2003@mail.ru>
2025-04-24 19:17:15 +0800

    feat: add pages

--- musicVibe/src/App.jsx
+++ musicVibe/src/App.jsx
@@ -1,9 +1,26 @@
 import { useState } from "react";
+
 import StartPage from "./pages/StartPage/StartPage.jsx";
+import LoginPage from "./pages/LoginPage/LoginPage.jsx";
+import RegisterPage from "./pages/RegisterPage/RegisterPage.jsx";
+import GenresPage from "./pages/GenresPage/GenresPage.jsx";
+import MusiciansPage from "./pages/MusiciansPage/MusiciansPage.jsx";
+import MainLayout from "./layouts/MainLayout/MainLayout.jsx";
+import HomePage from "./pages/HomePage/HomePage.jsx";
+
 import "./App.module.scss";
 
 function App() {
-  return <StartPage />;
+  // return <StartPage />;
+  // return <LoginPage />;
+  // return <RegisterPage />;
+  // return <GenresPage />;
+  // return <MusiciansPage />;
+  return (
+    <MainLayout>
+      <HomePage />
+    </MainLayout>
+  );
 }
 
 export default App;
