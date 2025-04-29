1d2ac5c8808774e019c39949e11a20480070205b
Alex06052003 <amm-2003@mail.ru>
2025-04-30 00:20:46 +0800

    feat: add router

--- musicVibe/src/pages/HomePage/HomePage.jsx
+++ musicVibe/src/pages/HomePage/HomePage.jsx
@@ -1,11 +1,34 @@
+import { useEffect, useState } from "react";
+import api from "@/api";
 import Section from "@/components/Section/Section.jsx";
+import Loader from "@/components/Loader/Loader.jsx";
 
-function HomePage({ songs }) {
+function HomePage() {
+  const [tracks, setTracks] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchTracks() {
+      try {
+        const res = await api.get("/tracks");
+        setTracks(res.data);
+      } catch (err) {
+        console.error("Ошибка при получении треков:", err);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchTracks();
+  }, []);
+
+  if (loading) return <Loader />;
+
   return (
     <>
-      <Section title="Рекомендовано для вас" link="#" songs={songs} />
-      <Section title="Новинки" link="#" songs={songs} />
-      <Section title="Чарт" link="#" songs={songs} isChart="true" />
+      <Section title="Рекомендовано для вас" link="#" tracks={tracks} />
+      <Section title="Новинки" link="#" tracks={tracks} />
+      <Section title="Чарт" link="#" tracks={tracks} isChart="true" />
     </>
   );
 }
