1703916f4bd751cf186af58a64c03698e78522e0
Alex06052003 <aam-2003@mail.ru>
2025-05-05 16:38:59 +0800

    feat: add active links

--- musicVibe/src/components/LayoutNavBtn/LayoutNavBtn.jsx
+++ musicVibe/src/components/LayoutNavBtn/LayoutNavBtn.jsx
@@ -4,7 +4,12 @@ import styles from "./LayoutNavBtn.module.scss";
 const LayoutNavBtn = ({ path, icon, label }) => {
   return (
     <li className={styles["aside__nav-item"]}>
-      <NavLink className={styles["aside__nav-link"]} to={path}>
+      <NavLink
+        className={({ isActive }) =>
+          `${styles["aside__nav-link"]} ${isActive ? styles["nav__link--active"] : ""}`
+        }
+        to={path}
+      >
         {icon}
         <p>{label}</p>
       </NavLink>
