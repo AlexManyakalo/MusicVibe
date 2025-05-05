b80c6f972b0536578c568fd6f5c00bd59341a5dd
Alex06052003 <aam-2003@mail.ru>
2025-05-05 16:16:44 +0800

    feat: add click

--- musicVibe/src/components/MenuBtn/MenuBtn.jsx
+++ musicVibe/src/components/MenuBtn/MenuBtn.jsx
@@ -1,7 +1,11 @@
 import styles from "./MenuBtn.module.scss";
 
-const MenuBtn = ({ label }) => {
-  return <button className={styles.menu__btn}>{label}</button>;
+const MenuBtn = ({ label, handlePlay }) => {
+  return (
+    <button className={styles.menu__btn} onClick={handlePlay}>
+      {label}
+    </button>
+  );
 };
 
 export default MenuBtn;
