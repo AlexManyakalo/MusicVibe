5d6d0239907eeac843da89fa5eb20d1fc4a35ec9
Alex06052003 <aam-2003@mail.ru>
2025-05-05 16:16:20 +0800

    feat: add player

--- musicVibe/src/components/ChartItem/ChartItem.jsx
+++ musicVibe/src/components/ChartItem/ChartItem.jsx
@@ -1,11 +1,41 @@
+import { useContext, useState, useEffect } from "react";
+import { PlayerContext } from "@/context/PlayerContext";
+import { formatTime } from "@/utils/formatTime.js";
 import { Link } from "react-router-dom";
 import styles from "./ChartItem.module.scss";
-import { PlayIcon, HeartIcon } from "@/components/Icons/icons.jsx";
+import { PlayIcon, PauseIcon, HeartIcon } from "@/components/Icons/icons.jsx";
 
 function ChartItem({ index, track }) {
-  function handlePlay() {
-    console.log(`Play`);
+  const { playTrack, togglePlayPause, currentTrack, isPlaying, audio } =
+    useContext(PlayerContext);
+
+  const [currentTime, setCurrentTime] = useState(0);
+  const isCurrent = currentTrack?.id === track.id;
+  const duration = track.duration || 0; // используем duration из базы
+  
+  const displayTime = isCurrent && audio ? currentTime : duration;
+
+  useEffect(() => {
+    if (!audio || currentTrack?.id !== track.id) return;
+
+    const handleTimeUpdate = () => {
+      setCurrentTime(audio.currentTime);
+    };
+
+    audio.addEventListener("timeupdate", handleTimeUpdate);
+
+    return () => {
+      audio.removeEventListener("timeupdate", handleTimeUpdate);
+    };
+  }, [audio, currentTrack, track]);
+
+  function handlePlay(e) {
+    e.preventDefault();
+
+    if (isCurrent) togglePlayPause();
+    else playTrack(track);
   }
+
   function handleLike() {
     console.log(`Like`);
   }
@@ -13,32 +43,29 @@ function ChartItem({ index, track }) {
   return (
     <li className={styles.chart__item} onClick={handlePlay}>
       <div className={styles.item__left}>
-        <p className={styles["item__left-num"]}>{index + 1}</p>
+        <div className={styles["item__left-num"]}>{index + 1}</div>
         <div className={styles["item__left-block"]}>
           <img
-            src={track.image}
+            src={track.imageUrl}
             alt="Превью песни"
             className={styles.block__image}
           />
-          <PlayIcon />
+          {isCurrent && isPlaying ? (
+            <PauseIcon className={styles.pause__btn} />
+          ) : (
+            <PlayIcon />
+          )}
         </div>
         <div className={styles["item__left-text"]}>
           <h3 className={styles["text__title"]}>
-            <Link
-              to={`/track/${track.id}`}
-              onClick={e => {
-                e.stopPropagation();
-              }}
-            >
+            <Link to={`/track/${track.id}`} onClick={e => e.stopPropagation()}>
               {track.title}
             </Link>
           </h3>
           <Link
             className={styles["text__link"]}
             to={`/musician/${track.artistId}`}
-            onClick={e => {
-              e.stopPropagation();
-            }}
+            onClick={e => e.stopPropagation()}
           >
             {track.artistName}
           </Link>
@@ -54,7 +81,7 @@ function ChartItem({ index, track }) {
         >
           <HeartIcon />
         </button>
-        <p className={styles["item__right-time"]}>02:33</p>
+        <p className={styles["item__right-time"]}>{formatTime(displayTime)}</p>
       </div>
     </li>
   );
