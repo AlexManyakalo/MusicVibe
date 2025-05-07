8e49ff0fee96c335522b0570c3d957fac24d3773
Alex06052003 <aam-2003@mail.ru>
2025-05-07 15:05:49 +0800

    feat: add ArrowBtn

---
+++ musicVibe/src/components/ArrowBtn/ArrowBtn.jsx
@@ -0,0 +1,12 @@
+import { ArrowIcon } from "@/components/Icons/icons.jsx";
+import styles from "./ArrowBtn.module.scss";
+
+function ArrowBtn() {
+  return (
+    <button>
+      <ArrowIcon />
+    </button>
+  );
+}
+
+export default ArrowBtn;
