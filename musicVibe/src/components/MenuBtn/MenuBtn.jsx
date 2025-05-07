af6f68f909f1ad38f560677b2e25b60c3c6c952a
Alex06052003 <aam-2003@mail.ru>
2025-05-07 15:06:33 +0800

    feat: add disabled btn

--- musicVibe/src/components/MenuBtn/MenuBtn.jsx
+++ musicVibe/src/components/MenuBtn/MenuBtn.jsx
@@ -1,7 +1,16 @@
 import styles from "./MenuBtn.module.scss";
 
-function MenuBtn({ label }) {
-  return <button className={styles.menu__btn}>{label}</button>;
+function MenuBtn({ label, disabled = false, type = "button", onClick }) {
+  return (
+    <button
+      className={`${styles.menu__btn} ${disabled ? styles.disabled : ""}`}
+      disabled={disabled}
+      type={type}
+      onClick={onClick}
+    >
+      {label}
+    </button>
+  );
 }
 
 export default MenuBtn;
