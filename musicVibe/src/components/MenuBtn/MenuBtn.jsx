465101361ee55cf8e55620af557bf0f9c6adc041
Alex06052003 <aam-2003@mail.ru>
2025-05-07 10:03:16 +0800

    feat: update MenuBtn

--- musicVibe/src/components/MenuBtn/MenuBtn.jsx
+++ musicVibe/src/components/MenuBtn/MenuBtn.jsx
@@ -1,11 +1,7 @@
 import styles from "./MenuBtn.module.scss";
 
-const MenuBtn = ({ label, handlePlay }) => {
-  return (
-    <button className={styles.menu__btn} onClick={handlePlay}>
-      {label}
-    </button>
-  );
-};
+function MenuBtn({ label }) {
+  return <button className={styles.menu__btn}>{label}</button>;
+}
 
 export default MenuBtn;
