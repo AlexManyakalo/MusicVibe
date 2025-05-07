b4aece8106deda61d511a61c1cfe51684b26381b
Alex06052003 <aam-2003@mail.ru>
2025-05-07 15:06:17 +0800

    feat: add validation zod

--- musicVibe/src/components/Input/Input.jsx
+++ musicVibe/src/components/Input/Input.jsx
@@ -1,12 +1,32 @@
 import styles from "./Input.module.scss";
 
-function Input({ type = "text", placeholder = "", isBottom = false }) {
+function Input({
+  type = "text",
+  placeholder = "",
+  isBottom = false,
+  value = "",
+  onChange,
+  name,
+  error = "",
+  required = false,
+  disabled = false,
+}) {
   return (
-    <input
-      className={`${styles.input} ${isBottom ? styles.input__bottom : ""}`}
-      type={type}
-      placeholder={placeholder}
-    />
+    <div className={styles.input__wrapper}>
+      <input
+        className={`${styles.input} ${isBottom ? styles.input__bottom : ""} ${
+          error ? styles.input__error : ""
+        }`}
+        type={type}
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        name={name}
+        required={required}
+        disabled={disabled}
+      />
+      {error && <span className={styles.input__error_message}>{error}</span>}
+    </div>
   );
 }
 
