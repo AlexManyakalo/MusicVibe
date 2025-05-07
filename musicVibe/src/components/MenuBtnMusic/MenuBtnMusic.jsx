9a819e1ab9f2d843d028acfe8c88271e3f2ddd07
Alex06052003 <aam-2003@mail.ru>
2025-05-07 10:03:42 +0800

    feat: add MenuBtnMusic component

---
+++ musicVibe/src/components/MenuBtnMusic/MenuBtnMusic.jsx
@@ -0,0 +1,17 @@
+import { MiniPlayIcon, MiniPauseIcon } from "@/components/Icons/icons.jsx";
+import styles from "./MenuBtnMusic.module.scss";
+
+function MenuBtnMusic ({ label, handlePlay, isPlaying = false }) {
+  return (
+    <button className={styles.menu__btn} onClick={handlePlay}>
+      {label}
+      {isPlaying ? (
+        <MiniPauseIcon className={styles.pause__btn} />
+      ) : (
+        <MiniPlayIcon className={styles.play__btn} />
+      )}
+    </button>
+  );
+};
+
+export default MenuBtnMusic;
