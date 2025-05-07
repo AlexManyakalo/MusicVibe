5f2eaefedaa6f2ca273153c6b15309b9dd4271eb
Alex06052003 <aam-2003@mail.ru>
2025-05-07 15:06:55 +0800

    feat: add link for page

--- musicVibe/src/pages/GenresPage/GenresPage.jsx
+++ musicVibe/src/pages/GenresPage/GenresPage.jsx
@@ -1,17 +1,20 @@
-import SelectionPage from "../../components/SelectionPage/SelectionPage";
+import SelectionPage from "@/components/SelectionPage/SelectionPage";
+import { useNavigate } from "react-router-dom";
 
 function GenresPage() {
+  const navigate = useNavigate();
   const genres = Array.from({ length: 16 }, (_, i) => ({
     id: i + 1,
     name: `Жанр ${i + 1}`,
   }));
 
-  const handleNext = selectedIds => {
+  function handleNext(selectedIds) {
     const selectedNames = genres
       .filter(g => selectedIds.includes(g.id))
       .map(g => g.name)
       .join(", ");
     console.log("Вы выбрали: " + selectedNames);
+    navigate("/musicians");
   };
 
   return (
