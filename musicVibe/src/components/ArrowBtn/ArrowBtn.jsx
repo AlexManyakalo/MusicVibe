005725a65929c9cd748a37e013977340d85a5cde
Alex06052003 <aam-2003@mail.ru>
2025-05-10 15:09:44 +0800

    feat: add navigate

--- musicVibe/src/components/ArrowBtn/ArrowBtn.jsx
+++ musicVibe/src/components/ArrowBtn/ArrowBtn.jsx
@@ -1,9 +1,23 @@
+import { useNavigate } from "react-router-dom";
 import { ArrowIcon } from "@/components/Icons/icons.jsx";
 import styles from "./ArrowBtn.module.scss";
 
-function ArrowBtn() {
+function ArrowBtn({ direction = "back" }) {
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    if (direction === "back") {
+      navigate(-1);
+    } else {
+      navigate(1);
+    }
+  };
+
   return (
-    <button>
+    <button
+      className={`${styles.arrow__btn} ${direction === "forward" ? styles.forward : ""}`}
+      onClick={handleClick}
+    >
       <ArrowIcon />
     </button>
   );
