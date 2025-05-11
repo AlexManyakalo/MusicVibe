ee1b0d656e7088d10ae9b8ba4794a598fea137ae
Alex06052003 <aam-2003@mail.ru>
2025-05-11 11:30:53 +0800

    refactor: rename files

---
+++ musicVibe/src/components/ArrowBtns/ArrowBtns.jsx
@@ -0,0 +1,23 @@
+import { useNavigate } from "react-router-dom";
+import { ArrowIcon } from "@/components/Icons/icons.jsx";
+import styles from "./ArrowBtns.module.scss";
+
+function ArrowBtns() {
+  const navigate = useNavigate();
+
+  return (
+    <div className={styles.arrow__btns}>
+      <button className={styles.arrow__btn} onClick={() => navigate(-1)}>
+        <ArrowIcon />
+      </button>
+      <button
+        className={`${styles.arrow__btn} ${styles.forward}`}
+        onClick={() => navigate(1)}
+      >
+        <ArrowIcon />
+      </button>
+    </div>
+  );
+}
+
+export default ArrowBtns;
