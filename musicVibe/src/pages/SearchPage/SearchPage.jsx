5943745ed424ef4f125d6ab3c133b6a5db1c04eb
Alex06052003 <aam-2003@mail.ru>
2025-05-13 11:47:19 +0800

    feat: add Search page

--- musicVibe/src/pages/SearchPage/SearchPage.jsx
+++ musicVibe/src/pages/SearchPage/SearchPage.jsx
@@ -1,7 +1,51 @@
+import { useEffect, useState } from "react";
+import api from "@/api";
+// Components
+import Input from "@/components/Input/Input";
+import Section from "@/components/Section/Section.jsx";
+import Loader from "@/components/Loader/Loader.jsx";
+// Styles
 import styles from "./SearchPage.module.scss";
 
 function SearchPage() {
-  return <h4>Странциа поиска</h4>;
+  const [tracks, setTracks] = useState([]);
+  const [search, setSearch] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchTracks() {
+      try {
+        const res = await api.get("/tracks");
+        setTracks(res.data);
+      } catch (err) {
+        console.error("Ошибка при получении треков:", err);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchTracks();
+  }, []);
+
+  // Обработка изменения поиска
+  function handleSearhChange(e) {
+    setSearch(e.target.value);
+  }
+
+  if (loading) return <Loader />;
+
+  return (
+    <>
+      <Input
+        placeholder="Трек, альбом, музыкант"
+        isSearch="true"
+        value={search}
+        onChange={handleSearhChange}
+      />
+      <Section title="Новинки" link="#" tracks={tracks} />
+      <Section title="Чарт" link="#" tracks={tracks} isChart="true" />
+    </>
+  );
 }
 
 export default SearchPage;
