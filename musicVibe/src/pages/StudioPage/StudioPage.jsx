ee293bbef941f276db610f00ebd55762a16d7ba8
Alex06052003 <aam-2003@mail.ru>
2025-05-13 12:47:51 +0800

    feat: add studio page

---
+++ musicVibe/src/pages/StudioPage/StudioPage.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import api from "@/api";
+// Components
+import Loader from "@/components/Loader/Loader.jsx";
+// Styles
+import styles from "./StudioPage.module.scss";
+
+function StudioPage() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchUser() {
+      setLoading(false);
+    }
+    fetchUser();
+  }, []);
+
+  if (loading) return <Loader />;
+
+  return <h4>Страница Студии</h4>;
+}
+
+export default StudioPage;
