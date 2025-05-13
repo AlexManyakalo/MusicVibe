5f404bafae8ca855425cecb5c8d4037cdfffb5f2
Alex06052003 <aam-2003@mail.ru>
2025-05-13 12:47:42 +0800

    feat: add support page

---
+++ musicVibe/src/pages/SupportPage/SupportPage.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import api from "@/api";
+// Components
+import Loader from "@/components/Loader/Loader.jsx";
+// Styles
+import styles from "./SupportPage.module.scss";
+
+function SupportPage() {
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchUser() {
+      setLoading(false);
+    }
+    fetchUser();
+  }, []);
+
+  if (loading) return <Loader />;
+
+  return <h4>Страница Поддержки</h4>;
+}
+
+export default SupportPage;
