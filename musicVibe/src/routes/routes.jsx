9b3a8841e771adb56079518f6a66393d8c188b16
Alex06052003 <aam-2003@mail.ru>
2025-05-13 12:48:20 +0800

    feat: add paths

--- musicVibe/src/routes/routes.jsx
+++ musicVibe/src/routes/routes.jsx
@@ -5,28 +5,39 @@ import LoadPage from "@/components/LoadPage/LoadPage";
 import MainLayout from "@/layouts/MainLayout/MainLayout.jsx";
 
 // Ленивая загрузка
+// Регистрация, авторизация
 const StartPage = lazy(() => import("@/pages/StartPage/StartPage.jsx"));
 const LoginPage = lazy(() => import("@/pages/LoginPage/LoginPage.jsx"));
 const RegisterPage = lazy(
   () => import("@/pages/RegisterPage/RegisterPage.jsx"),
 );
-const NotFoundPage = lazy(
-  () => import("@/pages/NotFoundPage/NotFoundPage.jsx"),
+// Жанры, музыканты
+const GenresPage = lazy(() => import("@/pages/GenresPage/GenresPage.jsx"));
+const MusiciansPage = lazy(
+  () => import("@/pages/MusiciansPage/MusiciansPage.jsx"),
 );
+// Главная, поиск, мои треки, сообщества
 const HomePage = lazy(() => import("@/pages/HomePage/HomePage.jsx"));
 const SearchPage = lazy(() => import("@/pages/SearchPage/SearchPage.jsx"));
 const MyMusicPage = lazy(() => import("@/pages/MyMusicPage/MyMusicPage.jsx"));
 const CommunityPage = lazy(
   () => import("@/pages/CommunityPage/CommunityPage.jsx"),
 );
-const GenresPage = lazy(() => import("@/pages/GenresPage/GenresPage.jsx"));
-const MusiciansPage = lazy(
-  () => import("@/pages/MusiciansPage/MusiciansPage.jsx"),
+// Профиль, подписка, студия, поддержка
+const ProfilePage = lazy(() => import("@/pages/ProfilePage/ProfilePage.jsx"));
+const StudioPage = lazy(() => import("@/pages/StudioPage/StudioPage.jsx"));
+const SubscribePage = lazy(
+  () => import("@/pages/SubscribePage/SubscribePage.jsx"),
 );
+const SupportPage = lazy(() => import("@/pages/SupportPage/SupportPage.jsx"));
+// Музыкант, трек
 const MusicianPage = lazy(
   () => import("@/pages/MusicianPage/MusicianPage.jsx"),
 );
 const TrackPage = lazy(() => import("@/pages/TrackPage/TrackPage.jsx"));
+const NotFoundPage = lazy(
+  () => import("@/pages/NotFoundPage/NotFoundPage.jsx"),
+);
 
 const AppRoutes = () => {
   return (
@@ -53,6 +64,22 @@ const AppRoutes = () => {
         <Route
           path="/community"
           element={<LoadPage Component={CommunityPage} />}
+        />
+        <Route
+          path="/profile/:id"
+          element={<LoadPage Component={ProfilePage} />}
+        />
+        <Route
+          path="/studio/:id"
+          element={<LoadPage Component={StudioPage} />}
+        />
+        <Route
+          path="/subscribe/:id"
+          element={<LoadPage Component={SubscribePage} />}
+        />
+        <Route
+          path="/support/:id"
+          element={<LoadPage Component={SupportPage} />}
         />
         <Route
           path="/musician/:id"
