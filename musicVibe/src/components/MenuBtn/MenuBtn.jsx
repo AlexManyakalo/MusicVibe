74d2593d26062dfd37a9a451ca54e7900ef076ea
Alex06052003 <aam-2003@mail.ru>
2025-05-13 16:40:00 +0800

    feat: add attribute & styles

--- musicVibe/src/components/MenuBtn/MenuBtn.jsx
+++ musicVibe/src/components/MenuBtn/MenuBtn.jsx
@@ -1,9 +1,9 @@
 import styles from "./MenuBtn.module.scss";
 
-function MenuBtn({ label, disabled = false, type = "button", onClick }) {
+function MenuBtn({ label, disabled = false, danger = false, type = "button", onClick }) {
   return (
     <button
-      className={`${styles.menu__btn} ${disabled ? styles.disabled : ""}`}
+      className={`${styles.menu__btn} ${disabled ? styles.disabled : ""} ${danger ? styles.danger : ""}`}
       disabled={disabled}
       type={type}
       onClick={onClick}
