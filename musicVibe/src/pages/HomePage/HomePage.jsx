b5dce6bc6505539189efbb622bbc566f66cc5f7e
Alex06052003 <aam-2003@mail.ru>
2025-05-14 12:18:22 +0800

    chore: add comments

--- musicVibe/src/pages/HomePage/HomePage.jsx
+++ musicVibe/src/pages/HomePage/HomePage.jsx
@@ -3,6 +3,11 @@ import api from "@/api";
 // Components
 import { Section, Loader } from "@/components/index.js";
 
+// TODO: 
+// На этой странице запросы должны быть на только необходимые треки
+// Например, 8 треков для рекомендаций, 8 треков для отслеживаемого и 8 треков чарта (всего 3 запроса)
+// Для дочерних страниц запросы должны быть на все треки
+
 function HomePage() {
   const [tracks, setTracks] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -27,7 +32,7 @@ function HomePage() {
   return (
     <>
       <Section title="Рекомендовано для вас" link="/recommend/1" tracks={tracks} />
-      <Section title="Отслеживаемое" link="#" tracks={tracks} />
+      <Section title="Отслеживаемое" link="/tracked/1" tracks={tracks} />
       <Section title="Чарт" link="/chart" tracks={tracks} isChart="true" />
     </>
   );
