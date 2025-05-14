c136d98999cbb32c1ba9fddcf7945716d155f21d
Alex06052003 <aam-2003@mail.ru>
2025-05-14 12:17:57 +0800

    feat: update TrackList component

--- musicVibe/src/components/TrackList/TrackList.jsx
+++ musicVibe/src/components/TrackList/TrackList.jsx
@@ -1,23 +1,25 @@
 import { useEffect, useState } from "react";
 import api from "@/api";
 // Components
-import { ArrowBtns, ChartItem, MusicCard, Loader } from "@/components/index.js";
+import { ArrowBtns, ChartItem, MusicCard } from "@/components/index.js";
 // Styles
 import styles from "./TrackList.module.scss";
 
-function TrackList() {
-  const [loading, setLoading] = useState(true);
-
-  useEffect(() => {
-    async function fetchUser() {
-      setLoading(false);
-    }
-    fetchUser();
-  }, []);
-
-  if (loading) return <Loader />;
-
-  return <h4>Страница Поддержки</h4>;
+function TrackList({ tracks, isChart = false }) {
+  return (
+    <>
+      <ArrowBtns />
+      <ul className={isChart ? styles.chart__list : styles.section__list}>
+        {tracks.map((track, index) =>
+          isChart ? (
+            <ChartItem key={track.id} index={index} track={track} />
+          ) : (
+            <MusicCard key={track.id} track={track} />
+          ),
+        )}
+      </ul>
+    </>
+  );
 }
 
 export default TrackList;
