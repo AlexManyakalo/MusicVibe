a353c23a5b9ef4a3e90defd477f4c73cee8bbc21
Alex06052003 <aam-2003@mail.ru>
2025-05-14 15:27:43 +0800

    feat: add Albums page

---
+++ musicVibe/src/pages/AlbumsPage/AlbumsPage.jsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from "react";
+import api from "@/api";
+// Components
+import { Loader, TrackList } from "@/components/index.js";
+// Styles
+import styles from "./AlbumsPage.module.scss";
+
+// TODO:
+// На этой странице запрос должен быть на все необходимые треки
+// Например, все 30 альбомов музыканта (всего 1 запрос)
+
+function AlbumsPage() {
+  const [tracks, setTracks] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function fetchTracks() {
+      try {
+        const res = await api.get("/tracks"); // TODO: изменить на запрос к трекам отслеживаемых ()
+        setTracks(res.data);
+      } catch (err) {
+        console.error("Ошибка при получении треков:", err);
+      } finally {
+        setLoading(false);
+      }
+    }
+
+    fetchTracks();
+  }, []);
+
+  if (loading) return <Loader />;
+
+  return <TrackList tracks={tracks} />;
+}
+
+export default AlbumsPage;
