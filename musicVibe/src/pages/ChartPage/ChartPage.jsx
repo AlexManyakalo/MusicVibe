592837731f898a108a8a3021d75db3ca76db6b08
Alex06052003 <aam-2003@mail.ru>
2025-05-14 12:18:09 +0800

    feat: update Chart page

--- musicVibe/src/pages/ChartPage/ChartPage.jsx
+++ musicVibe/src/pages/ChartPage/ChartPage.jsx
@@ -1,23 +1,36 @@
 import { useEffect, useState } from "react";
 import api from "@/api";
 // Components
-import { Loader } from "@/components/index.js";
+import { Loader, TrackList } from "@/components/index.js";
 // Styles
 import styles from "./ChartPage.module.scss";
 
+// TODO:
+// На этой странице запрос должен быть на все необходимые треки
+// Например, все 100 треков чарта
+
 function ChartPage() {
+  const [tracks, setTracks] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    async function fetchUser() {
-      setLoading(false);
+    async function fetchTracks() {
+      try {
+        const res = await api.get("/tracks"); // TODO: изменить на запрос к трекам чарта
+        setTracks(res.data);
+      } catch (err) {
+        console.error("Ошибка при получении треков:", err);
+      } finally {
+        setLoading(false);
+      }
     }
-    fetchUser();
+
+    fetchTracks();
   }, []);
 
   if (loading) return <Loader />;
 
-  return <h4>Страница Весь чарт</h4>;
+  return <TrackList tracks={tracks} isChart={true} />;
 }
 
 export default ChartPage;
