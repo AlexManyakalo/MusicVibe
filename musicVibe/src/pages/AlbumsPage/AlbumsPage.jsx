10da45c5ac4855b33aae0722d752a199c9c37259
Alex06052003 <amm-2003@mail.ru>
2025-05-16 22:13:08 +0800

    fix: names albums in musicians page

--- musicVibe/src/pages/AlbumsPage/AlbumsPage.jsx
+++ musicVibe/src/pages/AlbumsPage/AlbumsPage.jsx
@@ -2,35 +2,33 @@ import { useEffect, useState } from "react";
 import api from "@/api";
 // Components
 import { Loader, TrackList } from "@/components/index.js";
-// Styles
-import styles from "./AlbumsPage.module.scss";
 
 // TODO:
 // На этой странице запрос должен быть на все необходимые треки
 // Например, все 30 альбомов музыканта (всего 1 запрос)
 
 function AlbumsPage() {
-  const [tracks, setTracks] = useState([]);
+  const [albums, setAlbums] = useState([]);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
-    async function fetchTracks() {
+    async function fetchAlbums() {
       try {
-        const res = await api.get("/tracks"); // TODO: изменить на запрос к трекам отслеживаемых ()
-        setTracks(res.data);
+        const res = await api.get("/albums"); // TODO: изменить на запрос к альбомам рекомендованным ()
+        setAlbums(res.data);
       } catch (err) {
-        console.error("Ошибка при получении треков:", err);
+        console.error("Ошибка при получении альбомов:", err);
       } finally {
         setLoading(false);
       }
     }
 
-    fetchTracks();
+    fetchAlbums();
   }, []);
 
   if (loading) return <Loader />;
 
-  return <TrackList tracks={tracks} />;
+  return <TrackList albums={albums} />;
 }
 
 export default AlbumsPage;
